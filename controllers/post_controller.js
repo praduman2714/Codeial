d22c80b9949fd0be4fed4f88a174ee2e268df9be
praduman2714 <pradu14jsr@gmail.com>
2023-03-21 17:02:32 +0530

    added the feature where a user can post a content

---
+++ controllers/post_controller.js
@@ -0,0 +1,10 @@
+const Post = require('../models/post');
+
+module.exports.create = async function(req, res){
+    let post = await Post.create({
+        content : req.body.content,
+        user : req.body._id
+    });
+    // console.log(post);
+    return res.redirect('back');
+}
--- routes/post.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const postController = require('../controller/postController');
-
-const router = express.Router();
-
-router.get('/post', postController.post);
-
-module.exports = router;
